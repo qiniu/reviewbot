3467fd5ccb08c2bb3abfb40a0e510430cb5dbb0d
jichangjun <jinsdu@outlook.com>
2024-02-19 16:05:23 +0800

    add reportFormat property

--- config/config.go
+++ config/config.go
@@ -15,6 +15,13 @@ type Linter struct {
 	WorkDir string   `json:"workDir,omitempty"`
 	Command string   `json:"command,omitempty"`
 	Args    []string `json:"args,omitempty"`
+
+	// ReportFormat is the format of the report, if empty, use github_checks by default.
+	// e.g. "github_checks", "github_pr_review"
+	// For more details, see:
+	// github_checks: https://developer.github.com/v3/checks/runs/#create-a-check-run
+	// github_pr_review: https://developer.github.com/v3/pulls/reviews/#create-a-pull-request-review
+	ReportFormat string `json:"reportFormat,omitempty"`
 }
 
 // NewConfig returns a new Config.
@@ -29,6 +36,8 @@ func NewConfig(conf string) (Config, error) {
 		return nil, err
 	}
 
+	c = fixConfig(c)
+
 	return c, nil
 }
 
@@ -42,4 +51,26 @@ func (c Config) CustomLinterConfigs(org, repo string) map[string]Linter {
 	}
 
 	return nil
+}
+
+// fixConfig fix the config
+// 1. if linterConfig.Enable is nil, set it to true
+// 2. if linterConfig.ReportFormat is empty, set it to "github_checks"
+func fixConfig(c Config) Config {
+	for org, repoConfig := range c {
+		for repo, linterConfig := range repoConfig {
+			if linterConfig.Enable == nil {
+				enable := true
+				linterConfig.Enable = &enable
+			}
+
+			if linterConfig.ReportFormat == "" {
+				linterConfig.ReportFormat = "github_checks"
+			}
+
+			c[org][repo] = linterConfig
+		}
+	}
+
+	return c
 }
