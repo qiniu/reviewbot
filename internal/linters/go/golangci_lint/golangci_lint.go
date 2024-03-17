610354d748423a6b0fe2af3241a6c4a19e2f65b0
jichangjun <jinsdu@outlook.com>
2024-03-17 15:56:32 +0800

    fix(golangci-lint): ignore typecheck error

--- internal/linters/go/golangci_lint/golangci_lint.go
+++ internal/linters/go/golangci_lint/golangci_lint.go
@@ -1,6 +1,9 @@
 package golangci_lint
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/qiniu/reviewbot/internal/linters"
 	"github.com/qiniu/x/xlog"
 )
@@ -17,5 +20,18 @@ func golangciLintHandler(log *xlog.Logger, a linters.Agent) error {
 		a.LinterConfig.Args = append([]string{}, "run")
 	}
 
-	return linters.GeneralHandler(log, a, linters.GeneralParse)
+	return linters.GeneralHandler(log, a, golangciLintParse)
+}
+
+func golangciLintParse(log *xlog.Logger, output []byte) (map[string][]linters.LinterOutput, error) {
+	var lineParse = func(line string) (*linters.LinterOutput, error) {
+		if strings.HasSuffix(line, "(typecheck)") {
+			// refer: https://github.com/qiniu/reviewbot/issues/82#issuecomment-2002340788
+			return nil, fmt.Errorf("skip golangci-lint typecheck error: %s", line)
+		}
+
+		return linters.GeneralLineParser(line)
+	}
+
+	return linters.Parse(log, output, lineParse)
 }
