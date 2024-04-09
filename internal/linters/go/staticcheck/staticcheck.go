d159f4b4991de26f4ce0796fdf2d09759013c190
jichangjun <jinsdu@outlook.com>
2024-04-09 15:33:01 +0800

    refactor(filter): extract filters rules

--- internal/linters/go/staticcheck/staticcheck.go
+++ internal/linters/go/staticcheck/staticcheck.go
@@ -17,11 +17,7 @@
 package staticcheck
 
 import (
-	"regexp"
-	"strings"
-
 	"github.com/qiniu/reviewbot/internal/linters"
-	"github.com/qiniu/x/log"
 	"github.com/qiniu/x/xlog"
 )
 
@@ -39,73 +35,5 @@ func staticcheckHandler(log *xlog.Logger, a linters.Agent) error {
 		a.LinterConfig.Args = append([]string{}, "-debug.no-compile-errors=true", "./...")
 	}
 
-	return linters.GeneralHandler(log, a, staticcheckParse)
-}
-
-// staticcheckParse parses the output of staticcheck.
-func staticcheckParse(log *xlog.Logger, output []byte) (map[string][]linters.LinterOutput, error) {
-	results, err := linters.GeneralParse(log, output)
-	if err != nil {
-		return nil, err
-	}
-
-	finalResults := make(map[string][]linters.LinterOutput)
-
-	// special handling for SA5008 (unknown JSON option error)
-	// Background:
-	//  * https://github.com/qiniu/reviewbot/issues/24
-	tagRex := regexp.MustCompile(`unknown JSON option "(.*)" \(SA5008\)`)
-	for file, linterResults := range results {
-		var lintersCopy []linters.LinterOutput
-		for _, linter := range linterResults {
-			matches := tagRex.FindStringSubmatch(linter.Message)
-			if len(matches) == 2 && isGoZeroCustomTag(matches[1]) {
-				log.Warnf("ignore this error: %v", linter.Message)
-				continue
-			}
-			lintersCopy = append(lintersCopy, linter)
-		}
-		if len(lintersCopy) > 0 {
-			finalResults[file] = lintersCopy
-		}
-	}
-
-	return finalResults, nil
-}
-
-// isGoZeroCustomTag checks if the tag is a go-zero custom tag.
-// refer: https://go-zero.dev/en/docs/tutorials/go-zero/configuration/overview#tag-checksum-rule
-const (
-	goZeroDefaultOption  = "default"
-	goZeroEnvOption      = "env"
-	goZeroInheritOption  = "inherit"
-	goZeroOptionalOption = "optional"
-	goZeroOptionsOption  = "options"
-	goZeroRangeOption    = "range"
-)
-
-// FIXME(CarlJi): this function is a temporary solution for go-zero custom tag, see [#24](https://github.com/qiniu/reviewbot/issues/24)
-// expect to remove this function after staticcheck supports go-zero custom tag.
-func isGoZeroCustomTag(jsonOption string) bool {
-	var found bool
-	switch {
-	case strings.HasPrefix(jsonOption, goZeroDefaultOption):
-		found = true
-	case strings.HasPrefix(jsonOption, goZeroEnvOption):
-		found = true
-	case strings.HasPrefix(jsonOption, goZeroInheritOption):
-		found = true
-	case strings.HasPrefix(jsonOption, goZeroOptionalOption):
-		found = true
-	case strings.HasPrefix(jsonOption, goZeroOptionsOption):
-		found = true
-	case strings.HasPrefix(jsonOption, goZeroRangeOption):
-		found = true
-	}
-
-	if found {
-		log.Warnf("this tag %v seems belongs to go-zero, ignore it temporary, see [#24](https://github.com/qiniu/reviewbot/issues/24) for more information", jsonOption)
-	}
-
-	return found
+	return linters.GeneralHandler(log, a, linters.GeneralParse)
 }
