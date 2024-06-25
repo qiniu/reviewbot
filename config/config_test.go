382c56f6d67b572fd5d2fa0d5860f2c999d7cfd5
zhouxiaoliang <zhouxiaoliang@qiniu.com>
2024-06-25 19:37:06 +0800

    update ut

--- config/config_test.go
+++ config/config_test.go
@@ -103,6 +103,50 @@ customConfig: # custom config for specific orgs or repos
 				},
 			},
 		},
+		{
+			name:        "config with golangci-lint config path",
+			expectError: false,
+			rawConfig: `
+globalDefaultConfig: # global default settings, will be overridden by qbox org and repo specific settings if they exist
+  githubReportType: "github_check_run" # github_pr_review, github_check_run
+  golangciLintConfig: "linters-config/.golangci.yml"
+  javapmdcheckruleConfig: "linters-config/config/.bestpractices.xml"   
+  javastylecheckruleConfig: "linters-config/config/.sun_checks.xml" 
+
+customConfig: # custom config for specific orgs or repos
+  qbox: # github organization name
+    golangci-lint:
+      enable: true
+      args: ["run", "-D", "staticcheck"] # disable staticcheck globally since we have a separate linter for it
+
+  qbox/net-cache:
+    luacheck:
+      enable: true
+      workDir: "nginx" # only run in the nginx directory since there are .luacheckrc files in this directory
+`,
+			expected: Config{
+				GlobalDefaultConfig: GlobalConfig{
+					GithubReportType:         GithubCheckRuns,
+					GolangCiLintConfig:       "linters-config/.golangci.yml",
+					JavaPmdCheckRuleConfig:   "linters-config/config/.bestpractices.xml",
+					JavaStyleCheckRuleConfig: "linters-config/config/.sun_checks.xml",
+				},
+				CustomConfig: map[string]map[string]Linter{
+					"qbox": {
+						"golangci-lint": {
+							Enable: boolPtr(true),
+							Args:   []string{"run", "-D", "staticcheck"},
+						},
+					},
+					"qbox/net-cache": {
+						"luacheck": {
+							Enable:  boolPtr(true),
+							WorkDir: "nginx",
+						},
+					},
+				},
+			},
+		},
 	}
 
 	for _, tc := range testCases {
