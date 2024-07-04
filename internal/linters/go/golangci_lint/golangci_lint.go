d6778ce4f218339367996e3b4f574c1c9bae1240
jichangjun <jinsdu@outlook.com>
2024-07-04 08:56:12 +0800

    feat: support golangci-lint custom config

--- internal/linters/go/golangci_lint/golangci_lint.go
+++ internal/linters/go/golangci_lint/golangci_lint.go
@@ -2,9 +2,13 @@ package golangcilint
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/qiniu/reviewbot/internal/linters"
+	"github.com/qiniu/reviewbot/internal/lintersutil"
+	"github.com/qiniu/x/log"
 	"github.com/qiniu/x/xlog"
 )
 
@@ -17,7 +21,15 @@ func init() {
 }
 
 func golangciLintHandler(log *xlog.Logger, a linters.Agent) error {
-	a = args(a)
+	if len(a.LinterConfig.Command) == 0 || (len(a.LinterConfig.Command) == 1 && a.LinterConfig.Command[0] == lintName) {
+		// Default mode, automatically apply parameters.
+		a = argsApply(a)
+	} else if a.LinterConfig.ConfigPath != "" {
+		// Custom mode, only apply golangci-lint configuration if necessary.
+		path := configApply(a)
+		log.Infof("golangci-lint config prepared: %v", path)
+	}
+
 	return linters.GeneralHandler(log, a, parser)
 }
 
@@ -42,9 +54,9 @@ func parser(log *xlog.Logger, output []byte) (map[string][]linters.LinterOutput,
 	return linters.Parse(log, output, lineParser)
 }
 
-// args is used to set the default parameters for golangci-lint
+// argsApply is used to set the default parameters for golangci-lint
 // see: ./docs/website/docs/component/go/golangci-lint
-func args(a linters.Agent) linters.Agent {
+func argsApply(a linters.Agent) linters.Agent {
 	config := a.LinterConfig
 	if len(config.Command) == 0 || len(config.Command) > 1 || config.Command[0] != lintName {
 		return a
@@ -102,6 +114,7 @@ func args(a linters.Agent) linters.Agent {
 		newArgs = append(newArgs, "--print-issued-lines=false")
 	}
 	if !configFlag && config.ConfigPath != "" {
+		config.ConfigPath = configApply(a)
 		newArgs = append(newArgs, "--config", config.ConfigPath)
 	}
 
@@ -109,4 +122,65 @@ func args(a linters.Agent) linters.Agent {
 	a.LinterConfig = config
 
 	return a
+}
+
+// configApply is used to get the config file path based on rules as below:
+// 1. if the config file exists in current directory, return its absolute path.
+// 2. if the config file exists in the workDir directory, return its absolute path.
+// 3. if the config file exists in the repo linter ConfigPath.
+func configApply(a linters.Agent) string {
+	// refer to https://golangci-lint.run/usage/configuration/
+	// the default config file name is .golangci.yml, .golangci.yaml, .golangci.json, .golangci.toml
+	var golangciConfigFiles = []string{".golangci.yml", ".golangci.yaml", ".golangci.json", ".golangci.toml"}
+
+	// if the config file exists in the current directory, return its absolute path
+	for _, file := range golangciConfigFiles {
+		if path, exist := lintersutil.FileExists(file); exist {
+			return path
+		}
+	}
+
+	// if the config file exists in the workDir directory, return its absolute path
+	if a.LinterConfig.WorkDir != "" {
+		for _, file := range golangciConfigFiles {
+			if path, exist := lintersutil.FileExists(a.LinterConfig.WorkDir + "/" + file); exist {
+				return path
+			}
+		}
+	}
+
+	if a.LinterConfig.ConfigPath == "" {
+		return ""
+	}
+
+	path, exist := lintersutil.FileExists(a.LinterConfig.ConfigPath)
+	if !exist {
+		// if the config file is not found, these probably not configured.
+		// here we still return the original config path which may let user know the config is wrong.
+		return a.LinterConfig.ConfigPath
+	}
+
+	// WorkDir is the working directory of the linter, it is a temporary directory
+	currentDir := a.LinterConfig.WorkDir
+
+	// if the config file is outside the repo, copy it to the repo
+	if !strings.Contains(path, currentDir) {
+		// copy the config file to the repo
+		data, err := os.ReadFile(path)
+		if err != nil {
+			log.Warnf("failed to read config file: %v, err: %v", path, err)
+			return ""
+		}
+
+		var targetFile = filepath.Join(currentDir, filepath.Base(path))
+		if err := os.WriteFile(targetFile, data, 0o600); err != nil {
+			log.Warnf("failed to write config file: %v ,err: %v", targetFile, err)
+			return ""
+		}
+
+		log.Infof("copy config file from %v to %v", path, targetFile)
+		return targetFile
+	}
+
+	return path
 }
